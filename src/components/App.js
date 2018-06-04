d159841f25ccec4ef42e0c4910b2b3e60a0d516e
Olumide <odokedusi@sidmach.com>
2018-06-04 23:14:53 +0100

    Initial commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { HashRouter, Route } from "react-router-dom";
+import { HashRouter, Route, Switch } from "react-router-dom";
 
 //COMPONENTS
 import Header from "./header";
